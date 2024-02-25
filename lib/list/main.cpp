069d8f6999713ead906428a4da7eb2abe0aced7e
Andrew <an.veringe@gmail.com>
2024-02-25 18:23:00 +0500

    list + merge

--- lib/list/main.cpp
+++ lib/list/main.cpp
@@ -1,35 +1,13 @@
+// #include <list>
+#include "s21_list.h"
+#include <algorithm>
 #include <iostream>
-#include <list>
-#include <string>
+int main () {
 
-#include "s21_list.h"  // Убедитесь, что путь к вашему заголовочному файлу указан правильно
-
-int main() {
-  std::list<double> b {2};
-  // s21::list<int> a{1, 2, 3, 123, 1, 4};
-  // auto it = a.begin();
-  // (it++);
-  // (it++);
-  // (it++);
-  // (it++);
-
-
-
-
-  // (it++);
-  auto l = b.begin();
-  // auto bb = b.begin();
-  // bb--;
-  // bb--;
-  // bb--;
-  // l--;
-    std::cout << nullptr << std::endl;
-    std::cout << NULL << std::endl;
-  while (1) {
-    printf("%p\n", &(*l));
-    std::cout << (*l++) << std::endl;
-    // printf("%s\n", (*l++).c_str());
-    // std::cout << std::hex <<( l++) << std::endl;
-  }
-  // std::cout << *(it) << std::endl;
+    s21::list<int> a;
+    s21::list<int> b{13,15,-2,16,24,40};
+    a.merge(b);
+    for(auto it = a.begin(); it != a.end(); it++)
+        std::cout << *it << std::endl; 
+    return 0;
 }
